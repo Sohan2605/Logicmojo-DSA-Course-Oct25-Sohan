cc639439b42a70c7fdec27a5f084c2856a3c6e03
Sohan <sohanpatil05@gmail.com>
2026-02-12 10:25:10 +0530

    Array - trapping rain water problem

---
+++ Array/trappingwater.go
@@ -0,0 +1,23 @@
+func trap(height []int) int {
+    n:=len(height)
+    leftMax:=make([]int , n)
+    rightMax:=make([]int , n)
+
+    leftMax[0] = height[0]
+
+    
+
+    for i:=1; i<n; i++ {
+        leftMax[i]=max(leftMax[i-1],height[i])
+    }
+    rightMax[n-1] = height[n-1]
+    for i:=n-2; i>=0; i-- {
+        rightMax[i]=max(rightMax[i+1],height[i])
+    }
+    totalWater:=0
+    for i:=0; i<n; i++ {
+        totalWater+=min(leftMax[i],rightMax[i]) - height[i]
+    }
+
+    return totalWater;
+}
