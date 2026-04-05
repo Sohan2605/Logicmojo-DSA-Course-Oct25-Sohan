29c43a1d2ec2126419daf7afbb0de67eccc5b5df
Sohan <sohanpatil05@gmail.com>
2026-04-05 18:00:55 +0530

    removeduplicatesfromsortedarray

--- Array/removeDplicatesFromArray.go
+++ Array/removeDplicatesFromArray.go
@@ -1,19 +1,16 @@
 func removeDuplicates(nums []int) int {
-    	if len(nums) == 0 {
-		return 0
-	}
 
-	// slow pointer
-	i := 0
+   left := 0
+   right := 1
 
-	// fast pointer
-	for j := 1; j < len(nums); j++ {
-		if nums[j] != nums[i] {
-			i++
-			nums[i] = nums[j]
-		}
-	}
-
-	
-	return i + 1
+   for right < len(nums){
+    
+    if nums[left]!=nums[right]{
+        left++;
+        nums[left] , nums[right] = nums[right] , nums[left]
+    }
+    right++
+   } 
+   
+   return left+1
 }
