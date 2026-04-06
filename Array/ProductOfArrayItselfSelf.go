3a46f3513b2b84de32c2f7d7b367cb43732ba436
Sohan <sohanpatil05@gmail.com>
2026-04-06 10:36:58 +0530

    productofarrayitselfself

---
+++ Array/ProductOfArrayItselfSelf.go
@@ -0,0 +1,18 @@
+func productExceptSelf(nums []int) []int {
+    n:=len(nums)
+    result:= make([]int , len(nums))
+
+    result[0] = 1 
+    for i:=1; i<n;i++{
+        result[i] = result[i-1] * nums[i-1]
+    }
+
+    right:=1
+    for i:=n-1; i>=0;i--{
+        result[i] = result[i] * right
+
+        right = right * nums[i]
+    } 
+
+    return result;
+}
