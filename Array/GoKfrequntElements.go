1ea03dcc3515ce5a46bbd0ef52a7990e3bfb2724
Sohan <sohanpatil05@gmail.com>
2026-04-29 10:04:20 +0530

    optimal solution for k frequent

--- Array/GoKfrequntElements.go
+++ Array/GoKfrequntElements.go
@@ -23,3 +23,29 @@ func topKFrequent(nums []int, k int) []int {
 
     return result
 }
+
+
+//optimal solution
+
+func topKFrequent(nums []int, k int) []int {
+    
+    mp := make(map[int]int)
+
+    for _ , val := range nums{
+        mp[val]++
+    }
+
+    bucket := make([][]int ,  len(nums)+1)
+    
+    for key , val := range mp{
+        bucket[val] = append(bucket[val],key)
+    }
+    result:=[]int{}
+    for i:=len(bucket)-1; i>=0 && len(result) < k ; i--{
+        if len(bucket[i]) > 0{
+            result = append(result,bucket[i]...)
+        }
+    }
+
+    return result[:k]
+}
