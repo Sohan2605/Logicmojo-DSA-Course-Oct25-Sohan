5d8b4277ad260fd8b034f4d60c941625e1e40660
Sohan <sohanpatil05@gmail.com>
2026-05-07 09:12:58 +0530

    optimal solution for k frequent

--- Array/Slidingwindowmaximum.go
+++ Array/Slidingwindowmaximum.go
@@ -20,3 +20,31 @@ func maxSlidingWindow(nums []int, k int) []int {
 
     return result
 }
+
+
+//2nd time
+func maxSlidingWindow(nums []int, k int) []int {
+    
+    deque := []int{}
+    result := []int{}
+
+    for i:=0; i<len(nums); i++ {
+
+        if len(deque) > 0 && deque[0] == i-k{
+            deque = deque[1:]
+        } 
+ 
+        for len(deque) > 0 && nums[i] > nums[deque[len(deque)-1]]{
+            deque = deque[:len(deque)-1]
+        }
+        deque = append(deque , i)
+
+        if i>=k-1{
+            result = append(result , nums[deque[0]])
+        }
+
+    
+
+    }
+        return result
+}
